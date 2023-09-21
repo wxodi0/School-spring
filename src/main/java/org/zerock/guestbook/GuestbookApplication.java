5c3a67cd7fc0fc8d0c932721d3ee508e69a0d9a3
wxodi0 <teayang06@gmail.com>
2023-09-21 12:26:49 +0900

    Add BeanName in main

--- src/main/java/org/zerock/guestbook/GuestbookApplication.java
+++ src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -1,13 +1,29 @@
 package org.zerock.guestbook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
+@Slf4j
 public class GuestbookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GuestbookApplication.class, args);
-	}
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(GuestbookApplication.class, args);
+		String[] beanNames = applicationContext.getBeanDefinitionNames();
 
+//		List<String> list = new ArrayList<>();
+
+		Arrays.stream(beanNames)
+				.forEach(e -> {
+					log.info("Bean Name : {}" , e);
+				});
+	}
 }
