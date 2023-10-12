8e3f9eddbf78823c3228f7031d1758b033987402
wxodi0 <teayang06@gmail.com>
2023-10-12 16:30:33 +0900

    Fix codeComments

--- src/main/java/org/zerock/guestbook/GuestbookApplication.java
+++ src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -16,17 +16,17 @@ public class GuestbookApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(GuestbookApplication.class, args);
-		String[] beanNames = applicationContext.getBeanDefinitionNames();
-
-		Arrays.stream(beanNames)
-				.forEach(e -> {
-					log.info("Bean Name : {}" , e);
-				});
-		School school = new School();
-		applicationContext.getBean("school",School.class);
-		log.info("school : {}", school);
-
-		Restaurant restaurant = applicationContext.getBean("restaurant" , Restaurant.class);
-		log.info("restaurant : {}", restaurant);
+//		String[] beanNames = applicationContext.getBeanDefinitionNames();
+//
+//		Arrays.stream(beanNames)
+//				.forEach(e -> {
+//					log.info("Bean Name : {}" , e);
+//				});
+//		School school = new School();
+//		applicationContext.getBean("school",School.class);
+//		log.info("school : {}", school);
+//
+//		Restaurant restaurant = applicationContext.getBean("restaurant" , Restaurant.class);
+//		log.info("restaurant : {}", restaurant);
 	}
 }
