90ec657730f4fa10b7a98cccd08462848a24bb6a
wxodi0 <teayang06@gmail.com>
2023-10-13 23:00:46 +0900

    Add HotelServiceImpl

--- src/main/java/org/zerock/guestbook/sample/service/HotelServiceImpl.java
+++ src/main/java/org/zerock/guestbook/sample/service/HotelServiceImpl.java
@@ -11,6 +11,16 @@ public class HotelServiceImpl implements HotelService {
 
     private Restaurant restaurant;
 
+    public HotelServiceImpl(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Autowired
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+
     @Override
     public void print() {
         System.out.println("hotelServiceImpl");
