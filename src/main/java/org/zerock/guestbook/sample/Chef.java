5389be390914cb1b3e9b5bed059605554e030ed3
wxodi0 <teayang06@gmail.com>
2023-10-23 10:10:52 +0900

    Add annotation

--- src/main/java/org/zerock/guestbook/sample/Chef.java
+++ src/main/java/org/zerock/guestbook/sample/Chef.java
@@ -1,5 +1,9 @@
 package org.zerock.guestbook.sample;
 
-public class Chef {
+import lombok.ToString;
+import org.springframework.stereotype.Component;
 
+@ToString
+@Component
+public class Chef {
 }
