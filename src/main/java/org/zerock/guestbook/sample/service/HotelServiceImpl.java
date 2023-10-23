30339a8a1823e2abd2a47acb3aad28b75bc0d2ba
wxodi0 <teayang06@gmail.com>
2023-10-23 10:10:13 +0900

    Add school for di

--- src/main/java/org/zerock/guestbook/sample/service/HotelServiceImpl.java
+++ src/main/java/org/zerock/guestbook/sample/service/HotelServiceImpl.java
@@ -1,30 +1,35 @@
 package org.zerock.guestbook.sample.service;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 import org.zerock.guestbook.sample.Restaurant;
+import org.zerock.guestbook.sample.School;
 
 @Service
 @ToString
-public class HotelServiceImpl implements HotelService {
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@RequiredArgsConstructor
+public class HotelServiceImpl implements HotelService  {
 
-    private Restaurant restaurant;
+    private final Restaurant restaurant;
+    private final School school;
 
-    public HotelServiceImpl(Restaurant restaurant) {
+    public HotelServiceImpl(Restaurant restaurant, School school) {
         this.restaurant = restaurant;
+        this.school = school;
     }
-
-    @Autowired
-    public void setRestaurant(Restaurant restaurant) {
-        this.restaurant = restaurant;
-    }
-
 
     @Override
     public void print() {
-        System.out.println("hotelServiceImpl");
+        System.out.println("......hotelServiceImpl 1");
     }
 }
-
-
