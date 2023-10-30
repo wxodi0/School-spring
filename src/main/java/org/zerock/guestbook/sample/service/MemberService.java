1ef8ec873b8392948f26416c651101d41c3df72a
wxodi0 <teayang06@gmail.com>
2023-10-30 20:17:14 +0900

    Add service for Member

--- src/main/java/org/zerock/guestbook/sample/service/MemberService.java
+++ src/main/java/org/zerock/guestbook/sample/service/MemberService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.zerock.guestbook.sample.repository.MemberRepository;
 import org.zerock.guestbook.sample.dto.Member;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +16,18 @@ public class MemberService {
     @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+
+    public long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("존재하는 회원입니다.");
+                });
     }
 
     public Optional<Member> findMember(int id) {
