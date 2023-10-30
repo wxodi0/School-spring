00dcb1aa6397223b99ab1bc2c802bfb49710047a
wxodi0 <teayang06@gmail.com>
2023-10-30 12:22:40 +0900

    Add MemberController for MemberService

--- src/main/java/org/zerock/guestbook/sample/config/MemberController.java
+++ src/main/java/org/zerock/guestbook/sample/config/MemberController.java
@@ -1,30 +1,37 @@
 package org.zerock.guestbook.sample.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.guestbook.sample.dto.Member;
+import org.zerock.guestbook.sample.service.MemberService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
 @RequestMapping
 public class MemberController {
+    private final MemberService memberService;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @PostMapping("/member")
-    public String joinMember() {
+    public String joinMember(Member member) throws Exception {
+
         return null;
     }
 
     @GetMapping("/member/{id}")
-    public Member member() {
-        return null;
+    public Optional<Member> member(@PathVariable int id) {
+        return memberService.findMember(id);
     }
 
     @GetMapping("/member/all")
     public List<Member> memberList() {
-        return null;
+        return memberService.findMembers();
     }
 }
