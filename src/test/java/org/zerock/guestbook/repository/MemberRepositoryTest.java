0e1aec8c7d7906d9ac5cd34afe6fb0440601a608
wxodi0 <teayang06@gmail.com>
2023-10-30 20:16:15 +0900

    Add MemberRepositoryTest for test

--- src/test/java/org/zerock/guestbook/repository/MemberRepositoryTest.java
+++ src/test/java/org/zerock/guestbook/repository/MemberRepositoryTest.java
@@ -1,4 +1,23 @@
 package org.zerock.guestbook.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.guestbook.sample.dto.Member;
+import org.zerock.guestbook.sample.repository.MemberRepository;
+import org.zerock.guestbook.sample.service.MemberService;
+
+@SpringBootTest
 public class MemberRepositoryTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    void join() {
+        Member member = new Member();
+        member.setName("TaeYang");
+
+        Long saveId = memberService.join(member);
+    }
 }
