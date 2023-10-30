82d4d25c01c2e722c3c6d2c8ac3f9e0b3c11bad1
wxodi0 <teayang06@gmail.com>
2023-10-30 20:16:52 +0900

    Add MemberFrom

---
+++ src/main/java/org/zerock/guestbook/sample/config/MemberFrom.java
@@ -0,0 +1,12 @@
+package org.zerock.guestbook.sample.config;
+
+public class MemberFrom {
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
