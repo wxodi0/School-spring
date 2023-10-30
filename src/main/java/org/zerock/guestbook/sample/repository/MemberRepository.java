b5ee8b5479723e7914f120549645148a65e884f6
wxodi0 <teayang06@gmail.com>
2023-10-30 20:17:07 +0900

    Add repository for Member

--- src/main/java/org/zerock/guestbook/sample/repository/MemberRepository.java
+++ src/main/java/org/zerock/guestbook/sample/repository/MemberRepository.java
@@ -3,5 +3,15 @@ package org.zerock.guestbook.sample.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.guestbook.sample.dto.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(int id);
+
+    Optional<Object> findByName(String name);
+
+    List<Member> findAll();
 }
