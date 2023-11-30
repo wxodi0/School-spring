0c4d308ba47e53ad40d22979f9c3c0ab423c638e
wxodi0 <teayang06@gmail.com>
2023-11-30 14:44:44 +0900

    Fix builder&Getter

--- src/main/java/org/zerock/guestbook/dto/GuestbookDTO.java
+++ src/main/java/org/zerock/guestbook/dto/GuestbookDTO.java
@@ -1,52 +1,37 @@
 package org.zerock.guestbook.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class GuestbookDTO {
     private Long gno;
 
-    public Long getGno() {
-        return gno;
-    }
-
     public void setGno(Long gno) {
         this.gno = gno;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getContent() {
-        return content;
-    }
-
     public void setContent(String content) {
         this.content = content;
-    }
-
-    public String getWriter() {
-        return writer;
     }
 
     public void setWriter(String writer) {
         this.writer = writer;
     }
 
-    public LocalDateTime getRegDate() {
-        return regDate;
-    }
-
     public void setRegDate(LocalDateTime regDate) {
         this.regDate = regDate;
-    }
-
-    public LocalDateTime getModDate() {
-        return modDate;
     }
 
     public void setModDate(LocalDateTime modDate) {
